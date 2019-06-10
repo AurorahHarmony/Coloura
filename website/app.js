9966c251a85d8e03f9d3250dfd71dca6a949d2ba
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-06-10 15:44:44 +1200

    Added Speech API init with support check

--- website/app.js
+++ website/app.js
@@ -1,2 +1,10 @@
 const voiceInput = document.querySelector('#voiceInput');
 const speechOutput = document.querySelector('h1');
+
+//Create constant for speech API
+try {
+ const SpeechRecognition = window.speechRecognition || window.webkitSpeechRecognition;
+ const recognition = new SpeechRecognition();
+} catch (e) {
+    alert('Unsupported Browser! Please try Firefox or Chrome! (' + e + ')');
+}
